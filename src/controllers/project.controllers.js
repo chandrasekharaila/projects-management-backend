eb2befda4373fd8bc081202ed9ff6ef6d5763a75
Chandra Sekhar Aila <tarunaila007@gmail.com>
2025-10-25 17:59:49 +0530

    project routes

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -3,6 +3,8 @@ import { ProjectMember } from "../models/projectMembers.model.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import { UserRolesEnum } from "../utils/constants.js";
 import { asyncHandler } from "../utils/AsyncHandler.js";
+import ApiError from "../utils/ApiError.js";
+
 const addProject = asyncHandler(async (req, res) => {
   const { name, description } = req.body;
 
@@ -23,4 +25,80 @@ const addProject = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, project, "Project created successfully"));
 });
 
-export { addProject };
+const getProjects = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+
+  const projects = await ProjectMember.aggregate([
+    {
+      $match: {
+        user: userId,
+      },
+    },
+    {
+      $lookup: {
+        from: "Project",
+        localField: "project",
+        foreignField: "_id",
+        as: "projectDetails",
+        pipeline: [
+          {
+            $lookup: {
+              from: "ProjectMember",
+              localField: "_id",
+              foreignField: "project",
+              as: "projectMembers",
+            },
+          },
+          {
+            $addFields: {
+              members: {
+                $size: "$projectMembers",
+              },
+            },
+          },
+        ],
+      },
+    },
+    {
+      $unwind: "$projectDetails",
+    },
+    {
+      $project: {
+        projectDetails: {
+          _id: 1,
+          name: 1,
+          description: 1,
+          members: 1,
+          projectMembers: 0,
+        },
+        role: 1,
+        user: 0,
+        _id: 0,
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, projects, "Fetched the projects successfully"));
+});
+
+const getProjectById = asyncHandler(async (req, res) => {
+  const { projectId } = req.params;
+
+  if (!projectId) {
+    throw new ApiError(404, "project id is required");
+  }
+
+  const project = await Project.findById(projectId);
+
+  if (!project) {
+    throw new ApiError(400, "Project not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, project, "Project fetched successfully"));
+});
+
+export { addProject, getProjects, getProjectById };
--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -1,11 +1,42 @@
 import { Router } from "express";
 import { validate } from "../middlewares/validation.middlewares.js";
-import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { addProject } from "../controllers/project.controllers.js";
+import {
+  verifyJWT,
+  verifyProjectPermission,
+} from "../middlewares/auth.middleware.js";
+import {
+  addProject,
+  getProjects,
+  getProjectById,
+} from "../controllers/project.controllers.js";
 import { addProjectValidator } from "../validators/projects.validators.js";
+import { UserRolesEnum } from "../utils/constants.js";
 const router = Router();
 
 router
   .route("/add-project")
   .post(verifyJWT, addProjectValidator(), validate, addProject);
+
+router
+  .route("/get-projects")
+  .get(
+    verifyJWT,
+    verifyProjectPermission([
+      UserRolesEnum.PROJECT_ADMIN,
+      UserRolesEnum.MEMBER,
+      UserRolesEnum.ADMIN,
+    ]),
+    getProjects,
+  );
+router
+  .route("/get-project/:projectId")
+  .get(
+    verifyJWT,
+    verifyProjectPermission([
+      UserRolesEnum.PROJECT_ADMIN,
+      UserRolesEnum.MEMBER,
+      UserRolesEnum.ADMIN,
+    ]),
+    getProjectById,
+  );
 export default router;
